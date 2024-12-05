b0f7243be0229633341b89c517f1dab984951b8c
LawrenceHCj <15328709908@163.com>
2024-12-05 15:13:50 +0800

    modify file

--- cmaketry/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "test1.h"
-using namespace std;
-int main (){
-  cout <<"hello world"<<endl;
-   test1();
-   return 0;
-   }
--- cmaketry/src/test1.cpp
+++
@@ -1,7 +0,0 @@
-#include "test1.h"
-#include <iostream>
-using namespace std;
-
- void test1(){
- 	cout <<"this is test1"<<endl;
- 	}
