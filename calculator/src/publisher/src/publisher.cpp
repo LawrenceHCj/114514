4da8ebab2bb64c4431a594b7741149556a588598
LawrenceHCj <15328709908@163.com>
2025-01-14 09:01:52 +0800

    1.13 mission

--- calculator/src/publisher/src/publisher.cpp
+++ calculator/src/publisher/src/publisher.cpp
@@ -5,127 +5,99 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/int32.hpp"
-#include<iostream>
-using std::placeholders::_1;
+#include <iostream>
+
 using namespace std;
-using namespace std::chrono_literals;
+
 class PublisherNode : public rclcpp::Node
 {
-    public:
-        PublisherNode(): Node("Publisher_node"){
-            num1_publisher_ = this->create_publisher<std_msgs::msg::Int32>("num1_topic",10);
-            num2_publisher_ = this->create_publisher<std_msgs::msg::Int32>("num2_topic",10);
-            char_publisher_ = this->create_publisher<std_msgs::msg::String>("char_topic",10);
-            NeedType_publisher_ = this->create_publisher<std_msgs::msg::Int32>("NeedType_topic",10);
-            flag_ = false;
+public:
+    PublisherNode() : Node("Publisher_node"), num1(0), num2(0), type('+'), needType(0)
+    {
+        // 创建发布者
+        num1_publisher_ = this->create_publisher<std_msgs::msg::Int32>("num1_topic", 10);
+        num2_publisher_ = this->create_publisher<std_msgs::msg::Int32>("num2_topic", 10);
+        char_publisher_ = this->create_publisher<std_msgs::msg::String>("char_topic", 10);
+        NeedType_publisher_ = this->create_publisher<std_msgs::msg::Int32>("NeedType_topic", 10);
+    }
+
+    void setCalculationParams(int num1, int num2, char type, int needType)
+    {
+        this->num1 = num1;
+        this->num2 = num2;
+        this->type = type;
+        this->needType = needType;
+    }
+
+    void publishMessages()
+    {
+        auto int_message1 = std_msgs::msg::Int32();
+        auto int_message2 = std_msgs::msg::Int32();
+        auto char_message = std_msgs::msg::String();
+        auto NeedType_message = std_msgs::msg::Int32();
+
+        int_message1.data = num1;
+        int_message2.data = num2;
+        char_message.data = string(1, type);  // 转换 char 为 string 发送
+        NeedType_message.data = needType;
+
+        // 发布消息
+        if(needType == 1){num1_publisher_->publish(int_message1);
+        num2_publisher_->publish(int_message2);
+        char_publisher_->publish(char_message);
+        NeedType_publisher_->publish(NeedType_message);
+
+        std::cout << "Published: " << num1 << " " << num2 << " " << type << " " << needType << std::endl;}
+        else{
+            NeedType_publisher_->publish(NeedType_message);
         }
+    }
 
+private:
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr num1_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr num2_publisher_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr char_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr NeedType_publisher_;
 
+    int num1, num2, needType;
+    char type;
+};
 
-        void PublishNumber(int num1,int num2,int type){
-                auto int_message1 = std_msgs::msg::Int32(),int_message2 = std_msgs::msg::Int32();
-                auto char_message = std_msgs::msg::String();
-                auto NeedType_message = std_msgs::msg::Int32();
-                int_message1.data = num1;
-                int_message2.data = num2;
-                char_message.data = type;
-                NeedType_message.data = needType;
-                NeedType_publisher_->publish(NeedType_message);
-                num1_publisher_->publish(int_message1);
-                num2_publisher_->publish(int_message2); 
-                char_publisher_->publish(char_message);
-        }
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
 
-        void publishNeed(){
-            // if(!flag_){
-                std::cout<<"choose your need"<<std::endl;
-                cout<<"1.getNumber"<<endl<<"2.type"<<endl<<"3.check history"
-                <<endl<<"4.delete history"<<endl;
-                int choice;
-                cin>>choice;
-                if(choice==1){
-                    needType = 1;
-                    Need1();
-                    Need2();
-                    PublishNumber(num1,num2,type);
-                    
-                }
-                else if(choice==2){
-                    needType = 2;
-                    Need2();
-                    Need1();
-                    PublishNumber(num1,num2,type);
-                }
-                else if(choice==3){
-                    needType = 3;
-                    auto NeedType_message = std_msgs::msg::Int32();
-                    NeedType_message.data = needType;
-                    NeedType_publisher_->publish(NeedType_message);
-                    
-                }
-                else if(choice==4){
-                    needType = 4;
-                    auto NeedType_message = std_msgs::msg::Int32();
-                    NeedType_message.data = needType;
-                    NeedType_publisher_->publish(NeedType_message);
+    auto publisher_node = std::make_shared<PublisherNode>();
 
-                }
-                
-                else{
-                    cout<<"Invalid choice"<<endl;
-                }
-                // flag_ = true;
-            // }
+    // 设置初始的数值和计算类型
+    publisher_node->setCalculationParams(10, 5, '+', 1);
 
-        }
-    private:
-        int needType;
-        int num1,num2;
+    while (rclcpp::ok())
+    {
+        // 提示用户输入
+        std::cout << "Enter the parameters for publishing:" << std::endl;
+        std::cout << " Enter need type (1: Calculate, 2: History, 3: Clear History)" << std::endl;
+
+        int num1, num2, needType;
         char type;
-        void Need1(){
-            std::cout<<"Enter the number"<<endl;
-           
-            cin>>num1>>num2;
-            
-        }
-        void Need2(){
-            cout<<"choose your calculate type"<<endl;
-            bool flag = true;
-            cin>>type;
-            while(flag){if(type=='+'){
-                flag = false;
-            }
-            else if(type=='-'){
-               flag = false;
-            }
-            else if(type=='*'){
-                flag = false;
-            }
-            else if(type=='/'){
-                flag = false;
-            }
-            else{
-                cout<<"Invalid type Reput"<<endl;
-                flag = true;
-            }
-            }
-            
-        }
-       
-
+        std::cin >> needType;
+        if(needType ==1){std::cout << "Enter num1 and num2: ";
+        std::cin >> num1 >> num2;
+        std::cout << "Enter calculation type (+, -, *, /): ";
+        std::cin >> type;}
+        
         
 
-        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr num1_publisher_;
-        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr num2_publisher_;
-        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr char_publisher_;
-        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr NeedType_publisher_;
-        bool flag_;
-};
-int main(int argc, char** argv) {
-    rclcpp::init(argc, argv);
-    auto publisher_node=(std::make_shared<PublisherNode>());
-    publisher_node->publishNeed();
-    rclcpp::spin(publisher_node);
+        // 更新参数
+        publisher_node->setCalculationParams(num1, num2, type, needType);
+
+        // 发布消息
+        publisher_node->publishMessages();
+
+        // 处理 ROS 事件循环
+        rclcpp::spin_some(publisher_node);
+    }
+
     rclcpp::shutdown();
     return 0;
 }
--- calculator/src/receiver/src/receiver.cpp
+++ calculator/src/receiver/src/receiver.cpp
@@ -1,3 +1,4 @@
+
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -5,76 +6,116 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/int32.hpp"
-#include<iostream>
-#include<vector>
-using namespace std;
-using std::placeholders::_1;
-class ReceiverNode:public rclcpp::Node{
-    vector<int> result;
-    int count =0;
-    private:
-        int needType;
-        int num1,num2;
-        char type;
-        rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr num1_subscription_;
-        rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr num2_subscription_;
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr char_subscription_;
-        rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr NeedType_subscription_;
-    public:
-        ReceiverNode():Node("Receiver_node"){
-            num1_subscription_ = this->create_subscription<std_msgs::msg::Int32>("num1_topic",10,std::bind(&ReceiverNode::num1_callback,this,_1));
-            num2_subscription_ = this->create_subscription<std_msgs::msg::Int32>("num2_topic",10,std::bind(&ReceiverNode::num2_callback,this,_1));
-            char_subscription_ = this->create_subscription<std_msgs::msg::String>("char_topic",10,std::bind(&ReceiverNode::char_callback,this,_1));
-            NeedType_subscription_ = this->create_subscription<std_msgs::msg::Int32>("NeedType_topic",10,std::bind(&ReceiverNode::NeedType_callback,this,_1));
-        }
-        void num1_callback(const std_msgs::msg::Int32::SharedPtr msg){
-            num1 = msg->data;
-            cout<<"I received num1: "<<msg->data<<endl;
-        }
-        void num2_callback(const std_msgs::msg::Int32::SharedPtr msg){
-            num2 = msg->data;
-            cout<<"I received num2: "<<msg->data<<endl;
-        }
-        void char_callback(const std_msgs::msg::String::SharedPtr msg){
-            type = msg->data[0];
-            cout<<"I received char: "<<msg->data<<endl;
-        }
-        void NeedType_callback(const std_msgs::msg::Int32::SharedPtr msg){
-            needType = msg->data;
-            if(needType == 1 || needType == 2){
-                if(type == '+'){
-                    result.push_back(num1+num2);
-                    count++;
-                }
-                else if(type == '-'){
-                    result.push_back(num1-num2);
-                    count++;
-                }
-                else if(type == '*'){
-                    result.push_back(num1*num2);
-                    count++;
-                }
-                else if(type == '/'){
-                    result.push_back(num1/num2);
-                    count++;
-                }
-            }
-            else if(needType == 3){
-                cout<<"History: ";
-                for(int i=0;i<count;i++){
-                    cout<<result[i]<<" ";
-                }
-                
-            }
-            else if(needType == 4){
-                result.clear();
-                count = 0;
-            }
-          
-        }
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+class ReceiverNode : public rclcpp::Node
+{
+public:
+    ReceiverNode() : Node("Receiver_node"), num1(0), num2(0), count(0), needType(0), type('+')
+    {
+        // 创建订阅者
+        num1_subscription_ = this->create_subscription<std_msgs::msg::Int32>(
+            "num1_topic", 10, std::bind(&ReceiverNode::num1_callback, this, std::placeholders::_1));
+        num2_subscription_ = this->create_subscription<std_msgs::msg::Int32>(
+            "num2_topic", 10, std::bind(&ReceiverNode::num2_callback, this, std::placeholders::_1));
+        char_subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "char_topic", 10, std::bind(&ReceiverNode::char_callback, this, std::placeholders::_1));
+        NeedType_subscription_ = this->create_subscription<std_msgs::msg::Int32>(
+            "NeedType_topic", 10, std::bind(&ReceiverNode::NeedType_callback, this, std::placeholders::_1));
+    }
+
+private:
+    // 处理 num1 订阅消息
+    void num1_callback(const std_msgs::msg::Int32::SharedPtr msg)
+    {
+        num1 = msg->data;
+        cout << "Received num1: " << num1 << endl;
+    }
+
+    // 处理 num2 订阅消息
+    void num2_callback(const std_msgs::msg::Int32::SharedPtr msg)
+    {
+        num2 = msg->data;
+        cout << "Received num2: " << num2 << endl;
+    }
+
+    // 处理 char 订阅消息
+    void char_callback(const std_msgs::msg::String::SharedPtr msg)
+    {
+        type = msg->data[0];  // 假设只发送一个字符表示操作符
+        cout << "Received char: " << type << endl;
+    }
+
+    // 处理 NeedType 订阅消息
+    void NeedType_callback(const std_msgs::msg::Int32::SharedPtr msg)
+    {
+        needType = msg->data;
+        
+        if (needType == 1)  // 计算
+        {
+            int result = 0;
+            if (type == '+')
+            {
+                result = num1 + num2;
+            }
+            else if (type == '-')
+            {
+                result = num1 - num2;
+            }
+            else if (type == '*')
+            {
+                result = num1 * num2;
+            }
+            else if (type == '/')
+            {
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                    cout << "Error: Division by zero!" << endl;
+            }
+
+            results.push_back(result);
+            count++;
+            cout << "Result: " << result << endl;
+        }
+        else if (needType == 2)  // 显示历史
+        {
+            if (results.empty()) {
+                cout << "No history available." << endl;
+            } else {
+                cout << "History: ";
+                for (int i = 0; i < count; i++)
+                {
+                    cout << results[i] << " ";
+                }
+                cout << endl;
+            }
+        }
+        else if (needType == 3)  // 清除历史
+        {
+            results.clear();
+            count = 0;
+            cout << "History cleared!" << endl;
+        }
+    }
+
+    // 订阅者
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr num1_subscription_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr num2_subscription_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr char_subscription_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr NeedType_subscription_;
+
+    // 数据成员
+    int num1, num2, needType, count;
+    char type;
+    vector<int> results;  // 存储计算结果的历史
 };
-int main(int argc, char** argv) {
+
+int main(int argc, char** argv)
+{
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<ReceiverNode>());
     rclcpp::shutdown();
